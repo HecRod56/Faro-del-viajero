b58a5978e46611a44d3b8a1bd559a0d3b32b0620
HectorRodriguez <rodriguez.guarneros.hector.daniel@gmail.com>
2026-05-01 11:33:22 -0600

    [FIX] Eliminación de app de busqueda y url de core

--- faro_del_viajero/urls.py
+++ faro_del_viajero/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('', include('chat.urls')),
     path('chat/', include('chat.urls')),
     path('', include('galeria.urls')),
-    path('', include('apps.busqueda.urls')),
     path('', include('apps.control_gastos.urls')),
     path('', include('apps.transporte.urls')),
 ]
